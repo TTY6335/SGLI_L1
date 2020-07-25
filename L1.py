2250436eb6b9cda8e9f8a0982a1bbe139e5d257b
Tetsuya <ogwtetsuya@gmail.com>
2020-07-25 16:22:17 +0900

    new L1

--- L1.py
+++ L1.py
@@ -2,62 +2,88 @@
 #L1B_VNR_NP 可視近赤外（非偏光）をバンド毎にtiffにする
 import h5py
 import numpy as np
-import os, sys
+import gdal, ogr, os, osr, sys
 import tifffile
 
-input_dir='./'
-input_file='L1B_VNR_NP_DATA.h5'
-input_data=input_dir+input_file
+if __name__ == '__main__':
 
-f=h5py.File(input_data,'r')
+########################
+#入力するファイルの情報#
+########################
+#ファイルパス
 
-lat_arr=np.array(f['Geometry_data']['Latitude'])
-lon_arr=np.array(f['Geometry_data']['Longitude'])
+	input_dir='../'
+#ファイル名
+	input_file='madagascar.h5'
 
-#GCPのメモをつくる。gdal_translateでつかう。
-memo="gcp_memo.txt"
-txt=open(memo,'w')
-for column in range(0,lat_arr.shape[0],10):
-    for row in range(0,lat_arr.shape[1],10): 
-        txt.write("-gcp "+str(row*10)+" "+str(column*10)+" "+str(lon_arr[column][row])+" "+str(lat_arr[column][row])+"\n")
-txt.close()
+########################
+#出力するファイルの情報#
+########################
+#ファイルパス	
+	output_file_path="./"
+#ファイル名
+	output_filename="madagascar_gcp.tif"
 
-#upper_left_lat=f['Geometry_data'].attrs['Upper_left_latitude']
-#upper_left_lon=f['Geometry_data'].attrs['Upper_left_longitude']
-#upper_right_lat=f['Geometry_data'].attrs['Upper_right_latitude']
-#upper_right_lon=f['Geometry_data'].attrs['Upper_right_longitude']
-#lower_left_lat=f['Geometry_data'].attrs['Lower_left_latitude']
-#lower_left_lon=f['Geometry_data'].attrs['Lower_left_longitude']
-#lower_right_lat=f['Geometry_data'].attrs['Lower_right_latitude']
-#lower_right_lon=f['Geometry_data'].attrs['Lower_right_longitude']
-#print("lat,lon")
-#print(upper_right_lat,upper_right_lon)
+	#hdf5のファイルを開く
+	input_data=input_dir+input_file
+	f=h5py.File(input_data,'r')
 
-band_list=['Lt_VN01','Lt_VN02','Lt_VN03','Lt_VN04','Lt_VN05','Lt_VN06','Lt_VN07','Lt_VN08','Lt_VN09','Lt_VN10','Lt_VN11']
-for band in band_list:
-    print(band)
-    band_image_arr=f['Image_data'][band]
-    print(band_image_arr.shape)
-    slope=f['Image_data'][band].attrs['Slope']
-    offset=f['Image_data'][band].attrs['Offset']
-    Slope_reflectance=f['Image_data'][band].attrs['Slope_reflectance']
-    Offset_reflectance=f['Image_data'][band].attrs['Offset_reflectance']
-    #numpyの行列にする
-    band_image_arr=np.array(band_image_arr,dtype='uint16')
-    #MSBを0にする
-    band_image_arr=np.where(band_image_arr>=32768,band_image_arr-32768,band_image_arr)
-    #15ビット目を0にする
-    band_image_arr=np.where(band_image_arr>=16384,band_image_arr-16384,band_image_arr)
-    #欠損値をnanで埋める 0で埋めたときよりもgdal_translateの処理が速くなる?
-    band_image_arr=np.where(band_image_arr==16383,np.nan,band_image_arr)
-#    #大気上端放射輝度を求める
-#    Lt=slope*band_image_arr+offset
-    #大気上端反射率を求める
-    Rt=Slope_reflectance*band_image_arr+Offset_reflectance
-    #出力
-    output_file_path="output/"
-    output_filename=input_file[:-3]+"_Rt"+band[2:]+".tif"
-    output_file=output_file_path+output_filename
-    tifffile.imsave(output_file,Rt)
+	lat_arr=np.array(f['Geometry_data']['Latitude'],dtype='float64')
+	lon_arr=np.array(f['Geometry_data']['Longitude'],dtype='float64')
+	
+	#GCPのリストを作る
+	gcp_list=[]
+	for column in range(0,lat_arr.shape[0],20):
+		for row in range(0,lat_arr.shape[1],20): 
+			gcp=gdal.GCP(lon_arr[column][row],lat_arr[column][row],0,row*10,column*10)
+			gcp_list.append(gcp)
+		
+
+#RGBの3バンドだけ抽出する
+	sgli_bands=['Lt_VN03','Lt_VN05','Lt_VN08']
+	rgb_list=[]
+
+	for sgli in (sgli_bands):
+		print(sgli)
+		band_image_arr=f['Image_data'][sgli]
+		slope=f['Image_data'][sgli].attrs['Slope']
+		offset=f['Image_data'][sgli].attrs['Offset']
+
+		#numpyの行列にする
+		band_image_arr=np.array(band_image_arr,dtype='uint16')
+		#MSBを0にする
+		band_image_arr=np.where(band_image_arr>=32768,band_image_arr-32768,band_image_arr)
+		#15ビット目を0にする
+		band_image_arr=np.where(band_image_arr>=16384,band_image_arr-16384,band_image_arr)
+		#欠損値をnanで埋める 0で埋めたときよりもgdal_translateの処理が速くなる?
+		band_image_arr=np.where(band_image_arr==16383,np.nan,band_image_arr)
+		#大気上端放射輝度を求める
+		Lt=slope*band_image_arr+offset
+		Lt=np.array(Lt,dtype='float64')
+		rgb_list.append(Lt)
+
+	print(Lt.shape)
+
+	#行数
+	row_size=Lt.shape[0]
+	#列数
+	col_size=Lt.shape[1]
+
+	#出力
+	dtype = gdal.GDT_Float64
+	output_file=output_file_path+output_filename
+	#バンド数
+	band=3
+	output = gdal.GetDriverByName('GTiff').Create(output_file,col_size,row_size,band,dtype)
+	output.GetRasterBand(1).WriteArray(rgb_list[2])
+	output.GetRasterBand(2).WriteArray(rgb_list[1])
+	output.GetRasterBand(3).WriteArray(rgb_list[0])
+	wkt = output.GetProjection()
+	output.SetGCPs(gcp_list,wkt)
+	#GCPを使ってEPSG4326に投影変換
+	output = gdal.Warp(output_file, output, dstSRS='EPSG:4326',tps = True,outputType=dtype)
+	output = None 	
+
+
 
 f.close()
